49920c0b6acd31cdfdfcbe89cdff1da2523dc16f
Daniel Nogueira <daniel.nogueira.filho@gmail.com>
2024-04-23 01:16:33 -0300

    Cards Principais incluídos

--- src/app/components/relatorio/relatorio.component.ts
+++ src/app/components/relatorio/relatorio.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CardsComponent } from "./cards/cards.component";
 
 @Component({
-  selector: 'app-relatorio',
-  standalone: true,
-  imports: [],
-  templateUrl: './relatorio.component.html',
-  styleUrl: './relatorio.component.css'
+    selector: 'app-relatorio',
+    standalone: true,
+    templateUrl: './relatorio.component.html',
+    styleUrl: './relatorio.component.css',
+    imports: [CardsComponent]
 })
 export class RelatorioComponent {
 
