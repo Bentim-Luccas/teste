792aefb31200e76ff2c425f7b68057a814797090
Daniel Nogueira <daniel.nogueira.filho@gmail.com>
2024-04-23 02:24:27 -0300

    Indicador adicionado

--- src/app/components/relatorio/relatorio.component.ts
+++ src/app/components/relatorio/relatorio.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CardsComponent } from "./cards/cards.component";
 import { CardOrcamentoComponent } from "./card-orcamento/card-orcamento.component";
 import { BreadcrumbComponent } from "./breadcrumb/breadcrumb.component";
+import { IndicadorComponent } from "./indicador/indicador.component";
 
 @Component({
     selector: 'app-relatorio',
     standalone: true,
     templateUrl: './relatorio.component.html',
     styleUrl: './relatorio.component.css',
-    imports: [CardsComponent, CardOrcamentoComponent, BreadcrumbComponent]
+    imports: [CardsComponent, CardOrcamentoComponent, BreadcrumbComponent, IndicadorComponent]
 })
 export class RelatorioComponent {
 
