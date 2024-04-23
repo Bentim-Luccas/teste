8ae8674a82de3bc48c6da63ae9738bfb6a10395e
Daniel Nogueira <daniel.nogueira.filho@gmail.com>
2024-04-23 01:57:00 -0300

    card orçamento adicionado

--- src/app/components/relatorio/relatorio.component.ts
+++ src/app/components/relatorio/relatorio.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CardsComponent } from "./cards/cards.component";
+import { CardOrcamentoComponent } from "./card-orcamento/card-orcamento.component";
+import { BreadcrumbComponent } from "./breadcrumb/breadcrumb.component";
 
 @Component({
     selector: 'app-relatorio',
     standalone: true,
     templateUrl: './relatorio.component.html',
     styleUrl: './relatorio.component.css',
-    imports: [CardsComponent]
+    imports: [CardsComponent, CardOrcamentoComponent, BreadcrumbComponent]
 })
 export class RelatorioComponent {
 
