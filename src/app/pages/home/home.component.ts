f9390ae19493d0794da39d48e793eaf11a32a599
Victor Fonseca <victorfonsecabarboza@gmail.com>
2024-05-07 16:18:44 -0300

    update project

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import ComentarioComponent from '../../components/comentario/comentario.component';
 import { TabsComponent } from '../../components/tabs/tabs.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { RouterModule } from '@angular/router';
-import { ProjetoComponent } from "../../components/projeto/projeto.component";
+
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [ComentarioComponent, TabsComponent, NavbarComponent, RouterModule, ProjetoComponent]
+    imports: [ComentarioComponent, TabsComponent, NavbarComponent]
 })
 export class HomeComponent {
 
