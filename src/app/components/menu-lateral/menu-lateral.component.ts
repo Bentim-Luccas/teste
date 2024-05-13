b289ee80e436ad14468f7e0ade1b1d43290ff51e
Adryano Chavier <adryano.chavieroficial@gmail.com>
2024-05-13 18:49:10 -0300

    Menu-lateral

--- src/app/components/menu-lateral/menu-lateral.component.ts
+++ src/app/components/menu-lateral/menu-lateral.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { EmpresaService } from '../../service/empresa.service';
+import { Empresa } from '../../interface/empresa';
 
 @Component({
   selector: 'app-menu-lateral',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
+  template: `<li *ngFor="let usuarios of usuarioresponse"></li>`,
   templateUrl: './menu-lateral.component.html',
   styleUrl: './menu-lateral.component.css'
 })
-export class MenuLateralComponent {
+export class MenuLateralComponent implements OnInit  {
 
+  
+
+
+
+  constructor ( private empresaservice :EmpresaService){}
+
+ empresa! : Empresa[];
+  ngOnInit(): void {
+    this.getEmpresa();
+  }
+  getEmpresa():void{
+    this.empresaservice.getEmpresa().subscribe({
+      next:(response) =>{
+        response && (this.empresa = response);
+      },
+        error: (error) => console.log(error),
+    })
+  }
+  
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import ComentarioComponent from '../../components/comentario/comentario.component';
 import { TabsComponent } from '../../components/tabs/tabs.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { MenuLateralComponent } from '../../components/menu-lateral/menu-lateral.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ComentarioComponent,TabsComponent,NavbarComponent],
+  imports: [ComentarioComponent,TabsComponent,NavbarComponent,MenuLateralComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
