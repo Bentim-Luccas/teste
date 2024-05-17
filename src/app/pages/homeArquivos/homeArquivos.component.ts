70a39b9a593849c649d68051c6555fc900be91e5
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2024-05-17 19:28:18 -0300

    17.05.2024

--- src/app/pages/homeArquivos/homeArquivos.component.ts
+++ src/app/pages/homeArquivos/homeArquivos.component.ts
@@ -3,12 +3,13 @@ import ComentarioComponent from '../../components/comentario/comentario.componen
 import { TabsComponent } from '../../components/tabs/tabs.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { MenuLateralComponent } from '../../components/menu-lateral/menu-lateral.component';
+import { ModalEditarProjetoComponent } from '../../components/modal-editar-projeto/modal-editar-projeto.component';
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './homeArquivos.component.html',
     styleUrl: './homeArquivos.component.css',
-    imports: [ComentarioComponent, TabsComponent, NavbarComponent, MenuLateralComponent]
+    imports: [ComentarioComponent, TabsComponent, NavbarComponent, MenuLateralComponent, ModalEditarProjetoComponent]
 })
 export class HomeComponent {
 
