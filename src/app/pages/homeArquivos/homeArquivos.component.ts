22c51b43a45481cfa884eb3ff80995a84cef7fd7
Victor Fonseca <victorfonsecabarboza@gmail.com>
2024-05-17 15:15:08 -0300

    correção na pasta homeArquivos

--- src/app/pages/homeArquivos/homeArquivos.component.ts
+++ src/app/pages/homeArquivos/homeArquivos.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import ComentarioComponent from '../../components/comentario/comentario.component';
 import { TabsComponent } from '../../components/tabs/tabs.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { ModalEditComponent } from "../../src/app/components/modal-edit/modal-edit.component";
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './homeArquivos.component.html',
     styleUrl: './homeArquivos.component.css',
-    imports: [ComentarioComponent, TabsComponent, NavbarComponent, ModalEditComponent]
+    imports: [ComentarioComponent, TabsComponent, NavbarComponent]
 })
 export class HomeComponent {
 
