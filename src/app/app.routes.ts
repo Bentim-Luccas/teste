4b986f1c7a8ddcb062cc6ffdd252735cfd5ea5bf
JoaoGSAlves <joaog.silalves@gmail.com>
2024-05-19 21:45:36 -0300

    Ajustando tela gerenciamento de arquivos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+//import { HomeComponent } from './pages/home/home.component';
 import { GerenciamentoArquivosComponent } from './pages/gerenciamento-arquivos/gerenciamento-arquivos.component'
+import { HomeComponent } from './pages/homeArquivos/homeArquivos.component';
 
-// import { HomeComponent } from './pages/homeArquivos/homeArquivos.component';
 
 
 export const routes: Routes = [
