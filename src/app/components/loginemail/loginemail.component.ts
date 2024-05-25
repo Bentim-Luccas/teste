c1d3dbb5574dded0b92ef4a8f68ec55a636ec5a0
Guilherme Valim <106542784+Gvalim17@users.noreply.github.com>
2024-05-25 17:41:36 -0300

    Atualizacao para melhor funcionamento da tela de login loginemail.component.ts

--- src/app/components/loginemail/loginemail.component.ts
+++ src/app/components/loginemail/loginemail.component.ts
@@ -2,9 +2,9 @@
   import { FormsModule, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
   import { CommonModule } from '@angular/common';
   import { Router } from '@angular/router';
-  import { AuthService } from '../../service/auth.service';
-  import { Email } from '../../interface/email';
-import { EmailTokenRecebido } from '../../interface/emailtoken_recebido';
+  import { AuthService } from '../services/auth.service';
+  import { Email } from '../model/email';
+import { EmailTokenRecebido } from '../model/emailtoken_recebido';
 import { catchError, throwError } from 'rxjs';
 
   @Component({
@@ -17,43 +17,32 @@ import { catchError, throwError } from 'rxjs';
   export class LoginemailComponent {
     emailFormControl = new FormControl('', [Validators.required, Validators.email]);
     credencias = false;
-    msgErro='ola '
+    msgErro='ola'
+    formEnviado = false;
   
     constructor(private router: Router, private authservice: AuthService) { }
   
     submitApp() {
-
-      console.log('Email FormControl:', this.emailFormControl.value);
-      console.log('Touched:', this.emailFormControl.touched);
-      console.log('Invalid:', this.emailFormControl.invalid);
-
-      this.emailFormControl.markAsTouched();
+      this.formEnviado = true;
       if (this.emailFormControl.valid) {
         this.msgErro=''
-        this.emailFormControl.setErrors({
-          credenciais:false
-        })
+        this.emailFormControl.setErrors(null)
+
         const email = new Email(this.emailFormControl.value);
         this.authservice.loginEmail(email).subscribe({
           next:(email: EmailTokenRecebido) => {
             if (email) {
               console.log(email);
-              if(email.usuario_email && email.usuario_token){
-                sessionStorage.setItem('email', email.usuario_email);
-                sessionStorage.setItem('token', email.usuario_token);
-                this.router.navigate(['logintoken']);
-              }
-              
+              sessionStorage.setItem('email', email.usuario_email);
+              sessionStorage.setItem('token', email.usuario_token);
+              this.router.navigate(['logintoken']);
             }
             
           },error:(error) => {                              
             this.msgErro = error.error.message
-            this.emailFormControl.setErrors({
-              credenciais:true
-            })
-
+            this.emailFormControl.setErrors({credenciais:true})
           }
-      })
+      });
       }
     }
   }
