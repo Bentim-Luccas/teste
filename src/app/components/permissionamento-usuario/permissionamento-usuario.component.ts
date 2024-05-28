d06c3786adacb5d7e4a71e5711e8e85dc6c9c988
adssenacgit <ads@senacrio.com.br>
2024-05-28 18:01:15 -0300

    inserção gerenciamento usuario e permissionamento

---
+++ src/app/components/permissionamento-usuario/permissionamento-usuario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-permissionamento-usuario',
+  standalone: true,
+  imports: [],
+  templateUrl: './permissionamento-usuario.component.html',
+  styleUrl: './permissionamento-usuario.component.css'
+})
+export class PermissionamentoUsuarioComponent {
+
+}
--- src/app/pages/gerenciamento-usuario/gerenciamento-usuario.component.ts
+++ src/app/pages/gerenciamento-usuario/gerenciamento-usuario.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { PermissionamentoUsuarioComponent } from '../../components/permissionamento-usuario/permissionamento-usuario.component';
+import { PerfilUsuarioComponent } from '../../components/perfil-usuario/perfil-usuario.component';
 
 @Component({
   selector: 'app-gerenciamento-usuario',
   standalone: true,
-  imports: [],
+  imports: [PerfilUsuarioComponent, PermissionamentoUsuarioComponent],
   templateUrl: './gerenciamento-usuario.component.html',
   styleUrl: './gerenciamento-usuario.component.css'
 })
