e0d6301e0ca0ff21d131f6fa25a2e680438263f6
adssenacgit <ads@senacrio.com.br>
2024-05-28 16:41:04 -0300

    genenciamento usuario

---
+++ src/app/pages/gerenciamento-usuario/gerenciamento-usuario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gerenciamento-usuario',
+  standalone: true,
+  imports: [],
+  templateUrl: './gerenciamento-usuario.component.html',
+  styleUrl: './gerenciamento-usuario.component.css'
+})
+export class GerenciamentoUsuarioComponent {
+
+}
