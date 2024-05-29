facfb93b08bed525a75f44891085fa647ffb604d
vtr363 <victor.m.rocha.vr@gmail.com>
2024-05-29 20:53:12 -0300

    desfaz alterações na tela de arquivo

--- src/app/components/gerenciamento-arquivos-components/barra-pesquisa/barra-pesquisa.component.ts
+++ src/app/components/gerenciamento-arquivos-components/barra-pesquisa/barra-pesquisa.component.ts
@@ -1,22 +1,14 @@
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
-import { MatDialog } from '@angular/material/dialog';
-import { ModalFiltroComponent } from '../modal-filtro/modal-filtro.component';
+
 
 @Component({
   selector: 'app-barra-pesquisa',
   standalone: true,
   imports: [MatIcon],
   templateUrl: './barra-pesquisa.component.html',
-  styleUrls: ['./barra-pesquisa.component.css']
+  styleUrl: './barra-pesquisa.component.css'
 })
 export class BarraPesquisaComponent {
 
-  constructor(public dialog: MatDialog) {}
-
-  openFilterDialog(): void {
-    this.dialog.open(ModalFiltroComponent, {
-      width: '680px'
-    });
-  }
 }
