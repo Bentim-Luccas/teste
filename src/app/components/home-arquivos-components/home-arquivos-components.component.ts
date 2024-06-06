4a218118206882cf6a4bb42763d5d8affd5fae21
Victor Fonseca <victorfonsecabarboza@gmail.com>
2024-06-06 19:39:39 -0300

    criação da tela de dashboard

---
+++ src/app/components/home-arquivos-components/home-arquivos-components.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-arquivos-components',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-arquivos-components.component.html',
+  styleUrl: './home-arquivos-components.component.css'
+})
+export class HomeArquivosComponentsComponent {
+
+}
--- src/app/pages/homeArquivos/homeArquivos.component.ts
+++ src/app/pages/homeArquivos/homeArquivos.component.ts
@@ -3,11 +3,14 @@ import ComentarioComponent from '../../components/comentario/comentario.componen
 import { TabsComponent } from '../../components/tabs/tabs.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { MenuLateralComponent } from '../../components/menu-lateral/menu-lateral.component';
+import { BreadcrumbComponent } from '../../components/breadcrumb/breadcrumb.component';
+import { BarraPesquisaProjetoComponent } from '../../components/home-arquivos-components/barra-pesquisa-projeto/barra-pesquisa-projeto.component';
+import { ModalCriarProjetoComponent } from '../../components/home-arquivos-components/modal-criar-projeto/modal-criar-projeto.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ComentarioComponent,TabsComponent,NavbarComponent,MenuLateralComponent],
+  imports: [ComentarioComponent,TabsComponent,NavbarComponent,MenuLateralComponent, BreadcrumbComponent, BarraPesquisaProjetoComponent, ModalCriarProjetoComponent],
   templateUrl: './homeArquivos.component.html',
   styleUrl: './homeArquivos.component.css'
 
