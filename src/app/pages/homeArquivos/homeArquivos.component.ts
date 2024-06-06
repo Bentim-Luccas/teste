b94b08985360734baf74210da277279a52f87ecd
Victor Fonseca <victorfonsecabarboza@gmail.com>
2024-06-05 21:07:06 -0300

    feat: atualização de acordo com a master

--- src/app/pages/homeArquivos/homeArquivos.component.ts
+++ src/app/pages/homeArquivos/homeArquivos.component.ts
@@ -3,7 +3,6 @@ import ComentarioComponent from '../../components/comentario/comentario.componen
 import { TabsComponent } from '../../components/tabs/tabs.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { MenuLateralComponent } from '../../components/menu-lateral/menu-lateral.component';
-<<<<<<< HEAD
 
 @Component({
   selector: 'app-home',
@@ -11,16 +10,7 @@ import { MenuLateralComponent } from '../../components/menu-lateral/menu-lateral
   imports: [ComentarioComponent,TabsComponent,NavbarComponent,MenuLateralComponent],
   templateUrl: './homeArquivos.component.html',
   styleUrl: './homeArquivos.component.css'
-=======
-import { ListaCompartilhadaComponent } from '../../components/lista-compartilhada/lista-compartilhada.component';
->>>>>>> f662d8b68eb434afe7719a844191ddb518e68e41
 
-@Component({
-    selector: 'app-home',
-    standalone: true,
-    templateUrl: './homeArquivos.component.html',
-    styleUrl: './homeArquivos.component.css',
-    imports: [ComentarioComponent, TabsComponent, NavbarComponent, MenuLateralComponent, ListaCompartilhadaComponent]
 })
 export class HomeComponent {
 
--- src/app/service/disciplina.service.ts
+++ src/app/service/disciplina.service.ts
@@ -5,79 +5,74 @@ import { Observable, catchError, throwError } from "rxjs";
 import { Disciplina } from "../interface/disciplina";
 
 const httpOptions = {
-    headers: new HttpHeaders({
-        'Content-Type': 'aplication/json'
-    })
+  headers: new HttpHeaders({
+    'Content-Type': 'aplication/json'
+  })
 };
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class DisciplinaService {
-    delete(id: number) {
-      throw new Error('Method not implemented.');
-    }
-    private readonly baseUrl: string;
+  private readonly baseUrl: string;
 
-    constructor(private http: HttpClient) {
-        this.baseUrl = environment.apiServer + 'disciplina';
-    }
+  constructor(private http: HttpClient) {
+    this.baseUrl = environment.apiServer + 'disciplina';
+  }
 
-    getDisciplina(): Observable<any>{
-      return this.http.get<any[]>(this.baseUrl).pipe(
-          catchError(this.handleError) // Handle potential errors
-        );
-    }
+  getDisciplina(): Observable<any> {
+    return this.http.get<any[]>(this.baseUrl).pipe(
+      catchError(this.handleError) // Handle potential errors
+    );
+  }
 
-    post(disciplina: Disciplina): Observable<Disciplina> {
-        return this.http.post<Disciplina>(this.baseUrl, disciplina, httpOptions).pipe(
-            catchError(this.handleError)
-        );
-    }
+  post(disciplina: Disciplina): Observable<Disciplina> {
+    return this.http.post<Disciplina>(this.baseUrl, disciplina, httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
 
-    findAll(): Observable<Disciplina[]> {
-        return this.http.get<Disciplina[]>(this.baseUrl).pipe(
-            catchError(this.handleError)
-        )
-    }
+  findAll(): Observable<Disciplina[]> {
+    return this.http.get<Disciplina[]>(this.baseUrl).pipe(
+      catchError(this.handleError)
+    )
+  }
 
-    findOne(id: number): Observable<Disciplina> {
-        return this.http.get<Disciplina>(`${this.baseUrl}/${id}`).pipe(
-            catchError(this.handleError) // Handle potential errors
-        );
-    }
+  findOne(id: number): Observable<Disciplina> {
+    return this.http.get<Disciplina>(`${this.baseUrl}/${id}`).pipe(
+      catchError(this.handleError) // Handle potential errors
+    );
+  }
 
-    patch(id: number, changes: Partial<Disciplina>): Observable<Disciplina> {
-        return this.http.patch<Disciplina>(`${this.baseUrl}/${id}`, changes, httpOptions).pipe(
-            catchError(this.handleError)
-        );
-    }
+  patch(id: number, changes: Partial<Disciplina>): Observable<Disciplina> {
+    return this.http.patch<Disciplina>(`${this.baseUrl}/${id}`, changes, httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
 
-    remove(id: number): Observable<void> {
-        return this.http.delete<void>(`${this.baseUrl}/${id}`).pipe(
-            catchError(this.handleError) // Handle potential errors
-        );
-    }
+  remove(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`).pipe(
+      catchError(this.handleError) // Handle potential errors
+    );
+  }
 
-    private handleError(error: any): Observable<never> {
-        let errorMessage: string;
-        if (error.error instanceof ErrorEvent) {
-            errorMessage = 'Ocorreu um erro: ' + error.error.message;
-        } else {
-            errorMessage = `O backend retornou o código ${error.status}: ${error.message}`
-        }
-        console.error(errorMessage);
-        return throwError(errorMessage);
+  private handleError(error: any): Observable<never> {
+    let errorMessage: string;
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = 'Ocorreu um erro: ' + error.error.message;
+    } else {
+      errorMessage = `O backend retornou o código ${error.status}: ${error.message}`
     }
-<<<<<<< HEAD
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
 
-    /* ======================= Services Baseados no Usuário ====================== */
+  /* ======================= Services Baseados no Usuário ====================== */
 
-    findDisciplinasDeProjetoIdDaEmpresaDoUsuarioId(idUsuario: number, idProjeto: number): Observable<Disciplina[]> {
-      return this.http.get<Disciplina[]>(`${this.baseUrl}/disciplinasDeProjetoIdDaEmpresaDoUsuarioId/${idUsuario}/${idProjeto}`).pipe(
-          catchError(this.handleError)
-      );
-    }
-=======
->>>>>>> f662d8b68eb434afe7719a844191ddb518e68e41
+  findDisciplinasDeProjetoIdDaEmpresaDoUsuarioId(idUsuario: number, idProjeto: number): Observable<Disciplina[]> {
+    return this.http.get<Disciplina[]>(`${this.baseUrl}/disciplinasDeProjetoIdDaEmpresaDoUsuarioId/${idUsuario}/${idProjeto}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
 }
