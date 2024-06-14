d5baf61806635e89f9fb987ea52a7de62e679245
Rodrigo Barreto <rodrigo.anjosb@gmail.com>
2024-06-14 10:55:33 -0300

    permissionamento: adição do modal para criar um usuário, ajustes na tabela de usuários e na treeview

--- src/app/pages/gerenciamento-usuario/gerenciamento-usuario.component.ts
+++ src/app/pages/gerenciamento-usuario/gerenciamento-usuario.component.ts
@@ -4,13 +4,14 @@ import { MenuLateralComponent } from "../../components/menu-lateral/menu-lateral
 import { BreadcrumbComponent } from "../../components/breadcrumb/breadcrumb.component";
 import { BarraPesquisaComponent } from "../../components/gerenciamento-arquivos-components/barra-pesquisa/barra-pesquisa.component";
 import { TabelaUsuariosComponent } from "../../components/gerenciamento-usuarios-components/tabela-usuarios/tabela-usuarios.component";
+import { CriacaoUsuarioModalButtonComponent, CriacaoUsuarioModalComponent } from '../../components/gerenciamento-usuarios-components/criacao-usuario-modal/criacao-usuario-modal.component';
 
 @Component({
     selector: 'app-gerenciamento-usuario',
     standalone: true,
     templateUrl: './gerenciamento-usuario.component.html',
     styleUrl: './gerenciamento-usuario.component.css',
-    imports: [NavbarComponent, MenuLateralComponent, BreadcrumbComponent, BarraPesquisaComponent, TabelaUsuariosComponent]
+    imports: [NavbarComponent, MenuLateralComponent, BreadcrumbComponent, BarraPesquisaComponent, TabelaUsuariosComponent, CriacaoUsuarioModalComponent, CriacaoUsuarioModalButtonComponent]
 })
 export class GerenciamentoUsuarioComponent {
 
