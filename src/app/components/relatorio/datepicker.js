8b59d4d6b47f7fe221d7d4533c8d5956fb2a3d0a
roger-roger-svg <rogercunhadeandrade@gmail.com>
2024-06-24 15:51:34 -0300

    tela de relatorio-arquivo implementada

--- src/app/components/relatorio/datepicker.js
+++
@@ -1,9 +0,0 @@
-import Datepicker from 'flowbite-datepicker';
-
-document.addEventListener('DOMContentLoaded', function() {
-  const datepickerEl = document.getElementById('datepickerId');
-
-  if (datepickerEl) {
-    new Datepicker(datepickerEl);
-  }
-});
