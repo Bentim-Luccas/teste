4aae1251afe6ca409e6d73bf5ae737e5fb08fdbd
Lud Galvão <99023858+LudGalvao@users.noreply.github.com>
2024-06-25 20:42:38 -0300

    Update auth.service.ts

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -58,8 +58,5 @@ export class AuthService  {
   verificarJWTToken(jwt:JWT_Token):Observable<Boolean>{
     return this.http.post<Boolean>(this.baseUrl+'auth/check_jwt',jwt)
   }
-
-  testeListaCompartilhada(obj:Object){
-    return this.http.post(this.baseUrl+'lista-compartilhada',obj)
-  }
+  
 }
