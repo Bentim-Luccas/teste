033be42c030c3a1db3f59ef393d2a10fb4244b5c
Matheus de Azevedo <matheus.eduardofc@hotmail.com>
2024-06-25 00:03:41 -0300

    Feature: Att HTML e CSS (Pat & Dudu)

--- src/app/pages/homeArquivos/homeArquivos.component.ts
+++ src/app/pages/homeArquivos/homeArquivos.component.ts
@@ -6,6 +6,7 @@ import { MenuLateralComponent } from '../../components/menu-lateral/menu-lateral
 import { BreadcrumbComponent } from '../../components/breadcrumb/breadcrumb.component';
 import { BarraPesquisaProjetoComponent } from '../../components/home-arquivos-components/barra-pesquisa-projeto/barra-pesquisa-projeto.component';
 import { HomeArquivosComponentsComponent } from "../../components/home-arquivos-components/home-arquivos-components.component";
+import { BreadcrumbHomeComponent } from '../../components/home-arquivos-components/breadcrumb-home/breadcrumb-home.component'
 import { AngularSplitModule } from 'angular-split';
 import { ButtonModalCriarProjeto } from "../../components/home-arquivos-components/modal-criar-projeto/button/button-modal-criar-projeto.component";
 
@@ -14,7 +15,7 @@ import { ButtonModalCriarProjeto } from "../../components/home-arquivos-componen
     standalone: true,
     templateUrl: './homeArquivos.component.html',
     styleUrl: './homeArquivos.component.css',
-    imports: [ComentarioComponent, TabsComponent, NavbarComponent, MenuLateralComponent, BreadcrumbComponent, BarraPesquisaProjetoComponent, HomeArquivosComponentsComponent, AngularSplitModule, ButtonModalCriarProjeto]
+    imports: [ComentarioComponent, TabsComponent, NavbarComponent, MenuLateralComponent, BreadcrumbComponent, BarraPesquisaProjetoComponent, HomeArquivosComponentsComponent, BreadcrumbHomeComponent, AngularSplitModule, ButtonModalCriarProjeto]
 })
 export class HomeComponent {
 
