963ecbf545f7fee0c475af47181d8b2ec265f50d
Mckz33 <mackenzie.max.rj@gmail.com>
2024-06-25 23:14:09 -0300

    adicionando withFetch no app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,15 +3,15 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { JwtModule } from '@auth0/angular-jwt';
 
 export function tokenGetter() {
   return sessionStorage.getItem("jwt");
 }
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideClientHydration(),provideHttpClient(),importProvidersFrom(
+  providers: [provideHttpClient(withFetch())
+,     provideRouter(routes, withComponentInputBinding()), provideClientHydration(),provideHttpClient(),importProvidersFrom(
     JwtModule.forRoot({
         config: {
             tokenGetter: tokenGetter,
