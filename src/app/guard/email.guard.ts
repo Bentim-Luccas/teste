562c9eca71392055f5a99bf00a9d88ce32c2b52b
Mckz33 <mackenzie.max.rj@gmail.com>
2024-06-25 22:54:33 -0300

    resolvendo falha na verificação do sessionStorage no navegador

--- src/app/guard/email.guard.ts
+++ src/app/guard/email.guard.ts
@@ -1,12 +1,14 @@
 import { CanActivateFn, Router } from '@angular/router';
 
 export const emailGuard: CanActivateFn = (route, state) => {
-  const check = sessionStorage.getItem("email")
-  const router = new Router();
-  if(check){
-    router.navigate(['logintoken'])
-    return false
+  // Verifique se estamos no ambiente do navegador
+  if (typeof window !== "undefined" && typeof sessionStorage !== "undefined") {
+    const check = sessionStorage.getItem("email");
+    const router = new Router();
+    if (check) {
+      router.navigate(['logintoken']);
+      return false;
+    }
   }
-  return true
-
+  return true;
 };
