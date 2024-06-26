62bad329e00c89c1daf6f735b022d54858624d58
Mckz33 <mackenzie.max.rj@gmail.com>
2024-06-26 00:41:03 -0300

    ajustando comportamento no session e alterando html para evitar conflitos

--- src/app/guard/email.guard.ts
+++ src/app/guard/email.guard.ts
@@ -1,14 +1,15 @@
 import { CanActivateFn, Router } from '@angular/router';
 
 export const emailGuard: CanActivateFn = (route, state) => {
-  // Verifique se estamos no ambiente do navegador
-  if (typeof window !== "undefined" && typeof sessionStorage !== "undefined") {
+  if (typeof window !== 'undefined' && typeof sessionStorage !== 'undefined') {
     const check = sessionStorage.getItem("email");
     const router = new Router();
     if (check) {
       router.navigate(['logintoken']);
       return false;
     }
+    return true;
+  } else {
+    return false; // Ou alguma outra lógica para ambientes que não sejam navegadores
   }
-  return true;
 };
