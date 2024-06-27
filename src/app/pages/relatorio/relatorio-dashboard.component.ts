a740778538a6b4a17d866c855f0f9cb49d553f75
JoaoGSAlves <joaog.silalves@gmail.com>
2024-06-27 17:26:08 -0300

    Ajuster do merge-grupo3

--- src/app/pages/relatorio/relatorio-dashboard.component.ts
+++ src/app/pages/relatorio/relatorio-dashboard.component.ts
@@ -8,61 +8,60 @@ import { BadgeAtivoComponent } from '../../components/relatorio/badge-ativo/badg
 import { CalendarComponent } from '../../components/relatorio/calendar/calendar.component';
 import { UserInfoComponent } from '../../components/relatorio/user-info/user-info.component';
 import { ArquivosRecentesComponent } from '../../components/relatorio/arquivos-recentes/arquivos-recentes.component';
-import { NavbarComponent } from "../../components/navbar/navbar.component";
-import { MenuLateralComponent } from "../../components/menu-lateral/menu-lateral.component";
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { MenuLateralComponent } from '../../components/menu-lateral/menu-lateral.component';
 import { first } from 'rxjs';
 import { Arquivo } from '../../interface/arquivo';
 import { ArquivoService } from '../../service/arquivo.service';
 
 @Component({
-    selector: 'app-relatorio',
-    standalone: true,
-    templateUrl: './relatorio-dashboard.component.html',
-    styleUrl: './relatorio-dashboard.component.css',
-    imports: [
-        CardsComponent,
-        BreadcrumbComponent,
-        IndicadorComponent,
-        DropdownComponent,
-        BadgeAtivoComponent,
-        CalendarComponent,
-        UserInfoComponent,
-        ArquivosRecentesComponent,
-        NavbarComponent,
-        MenuLateralComponent,
-        GraficoComponent,
-        BuscaComponent
-    ]
+  selector: 'app-relatorio',
+  standalone: true,
+  templateUrl: './relatorio-dashboard.component.html',
+  styleUrl: './relatorio-dashboard.component.css',
+  imports: [
+    CardsComponent,
+    BreadcrumbComponent,
+    IndicadorComponent,
+    DropdownComponent,
+    BadgeAtivoComponent,
+    CalendarComponent,
+    UserInfoComponent,
+    ArquivosRecentesComponent,
+    NavbarComponent,
+    MenuLateralComponent,
+    BuscaComponent,
+  ],
 })
-export class RelatorioComponent implements OnInit{
+export class RelatorioComponent implements OnInit {
+  listaArquivos: Arquivo[] = [];
+  listaArquivosAprovados: Arquivo[] = [];
+  listaArquivosReprovados: Arquivo[] = [];
+  listaArquivosPendentes: Arquivo[] = [];
 
-  listaArquivos: Arquivo[] = []
-  listaArquivosAprovados: Arquivo[] = []
-  listaArquivosReprovados: Arquivo[] = []
-  listaArquivosPendentes: Arquivo[] = []
-
-
-  constructor(private arquivoService: ArquivoService){}
+  constructor(private arquivoService: ArquivoService) {}
 
   ngOnInit(): void {
-    this.arquivoService.findAll().pipe(first()).subscribe(data => {
-      this.listaArquivos = data
-      console.log(data)
+    this.arquivoService
+      .findAll()
+      .pipe(first())
+      .subscribe((data) => {
+        this.listaArquivos = data;
+        console.log(data);
 
-      this.listaArquivos.forEach(arquivo => {
-        switch(arquivo.arquivo_status){
-          case 1:
-            this.listaArquivosAprovados.push(arquivo)
-            break;
-          case -1:
-            this.listaArquivosReprovados.push(arquivo)
-            break;
-          case 0:
-            this.listaArquivosPendentes.push(arquivo)
-            break;
-        }
-     });
-    })
- }
-
+        this.listaArquivos.forEach((arquivo) => {
+          switch (arquivo.arquivo_status) {
+            case 1:
+              this.listaArquivosAprovados.push(arquivo);
+              break;
+            case -1:
+              this.listaArquivosReprovados.push(arquivo);
+              break;
+            case 0:
+              this.listaArquivosPendentes.push(arquivo);
+              break;
+          }
+        });
+      });
+  }
 }
